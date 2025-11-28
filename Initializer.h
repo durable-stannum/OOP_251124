055fa139f396053f306b2fe23053a7408370682b
github-dohyun <34982988+github-dohyun@users.noreply.github.com>
2025-11-28 13:23:08 +0900

    Update Initializer.h

--- Initializer.h
+++ Initializer.h
@@ -19,17 +19,17 @@ private:
 
 public:
     Initializer(ifstream& fin, Interface& uiInput);
-    ~Initializer(); // [추가] 메모리 해제를 위한 소멸자
+    ~Initializer();
 
     void run();
     map<string, Bank*>& getAllBanks() { return allBanks; }
     
-    // [수정] const string& 적용 및 안전성 강화
+    //
     ATM* findATMBySerialNumber(const string& serialNumberInput);
     Bank* findBankByCardNumber(const string& cardNumberInput); 
     Account* findAccountPtrByCardNumber(const string& cardNumberInput);
 
-    // [추가] REQ 10.1 스냅샷 출력 함수
+    // 스냅샷 출력 함수
     void printSnapshot();
 };
 
