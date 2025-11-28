a44112e795e0a83b868463afe2ca38515b46db0e
github-dohyun <34982988+github-dohyun@users.noreply.github.com>
2025-11-28 13:55:34 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,16 @@ using namespace std;
 
 // 전역 변수 정의
 string language = "Unselected";
-Initializer* globalInitializer = nullptr; // [추가]
+Initializer* globalInitializer = nullptr;
 
 int main() {
 	Interface ui;
 
-	// 2. 파일 열기
+	//파일 열기
 	ui.displayMessage("InitializationStart");
 	string filePath;
 	getline(cin, filePath);
 
-	// 스냅샷 기능 테스트를 위해 여기서 / 입력 체크를 할 수도 있지만, 
-	// 파일 경로는 보통 그냥 받으므로 넘어갑니다.
 
 	ifstream fin(filePath);
 	if (!fin.is_open()) { 
@@ -28,11 +26,11 @@ int main() {
 	}
 	ui.displayMessage("FileOpenSuccess"); 
 
-	// 4. Initializer 생성 및 전역 포인터 연결
+	//Initializer 생성 및 전역 포인터 연결
 	Initializer* init = new Initializer(fin, ui); 
-	globalInitializer = init; // [추가] 전역 변수에 주소 저장
+	globalInitializer = init; //전역 변수에 주소 저장
 
-	// 5. 실행
+	//실행
 	init->run();
 
 	delete init;
