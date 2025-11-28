85224d590cf010accc64b948c9ac529835a3ec65
github-dohyun <34982988+github-dohyun@users.noreply.github.com>
2025-11-28 13:32:30 +0900

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -2,13 +2,13 @@
 #define TRANSACTION_H
 
 #include <string>
-#include "ATM.h" // CashDenominations 정의를 위해 필요
+#include "ATM.h"
 
 using namespace std;
 
 class Session; // 전방 선언
 
-// [추가] 거래 타입을 명확하게 구분하기 위한 Enum Class
+//거래 타입을 명확하게 구분하기 위한 Enum Class
 enum class TransactionType {
     DEPOSIT,        // 입금
     WITHDRAWAL,     // 출금
@@ -30,7 +30,7 @@ public:
 
     int getTransactionID() const;
 
-    // [수정] string 대신 Enum 사용
+    //string 대신 Enum 사용
     long calculateFee(TransactionType type, const string& destBankName = "") const;
     
     bool collectFee(long fee, CashDenominations& outFeeCash);
