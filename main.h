8138568770927bd8e767d45bec23fa4114adf080
github-dohyun <34982988+github-dohyun@users.noreply.github.com>
2025-11-28 13:40:08 +0900

    Update main.h

--- main.h
+++ main.h
@@ -8,6 +8,6 @@ class Initializer; // 전방 선언
 
 // 전역 변수 선언
 extern string language;
-extern Initializer* globalInitializer; // [추가] 스냅샷 출력을 위한 전역 포인터
+extern Initializer* globalInitializer; //스냅샷 출력을 위한 전역 포인터
 
 #endif
